2e33b1b92cda35cea20aa4c231dc28257c08707d
naiaero <salsabilanailafahdi26@gmail.com>
2025-12-06 14:12:12 +0800

    .

--- src/CMD_Version.java
+++ src/CMD_Version.java
@@ -1,6 +1,6 @@
 public class CMD_Version implements CLI_Command {
     public void execute(String[] args) {
-        System.out.println("NetTracer IOS Version 1.1 (Day 2 Build)");
+        System.out.println("NetTracer IOS Version 1.1");
         System.out.println("Created by Dev Team A-E");
     }
 
